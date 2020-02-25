13106aae3f3b93d0ea93e77fa9086408300fb6a9
yoo-mech <usam4760@gmail.com>
2020-02-25 23:08:26 +0900

    new file

--- lib/screens/home/event_page/models/movie.dart
+++ lib/screens/home/event_page/models/movie.dart
@@ -1,4 +1,4 @@
-import 'cinema.dart';
+
 
 class Movie {
   String title;
@@ -11,10 +11,9 @@ class Movie {
   String posterPath;
   String backgroundColor;
   String backdropImage;
-  List<Cinema> cinemas;
 
-  Movie(
-      this.title,
+
+  Movie (this.title,
       this.duration,
       this.rate,
       this.position,
@@ -23,30 +22,20 @@ class Movie {
       this.plot,
       this.posterPath,
       this.backgroundColor,
-      this.backdropImage,
-      this.cinemas);
+      this.backdropImage);
+
   Movie.fromJson(Map<String, dynamic> moviesJson) {
-    Movie(
-        title = moviesJson['title'],
-        duration = moviesJson['duration'],
-        rate = moviesJson['rate'],
-        position = moviesJson['position'],
-        releaseDate = moviesJson['release-date'],
-        shortInfo = moviesJson['short-info'],
-        plot = moviesJson['plot'],
-        posterPath = moviesJson['poster_path'],
-        backgroundColor = moviesJson['backgroundColor'],
-        backdropImage = moviesJson['backdrop_image'],
-        cinemas = _getCinemas(moviesJson['cinemas']));
-  }
-
-  List<Cinema> _getCinemas(List<dynamic> moviesJson) {
-    List<Cinema> savedCinemas = [];
-
-    moviesJson.forEach((cinema) {
-      Cinema cinemaItem = Cinema.fromJson(cinema);
-      savedCinemas.add(cinemaItem);
-    });
-    return savedCinemas;
+    Movie (
+      title = moviesJson['title'],
+      duration = moviesJson['duration'],
+      rate = moviesJson['rate'],
+      position = moviesJson['position'],
+      releaseDate = moviesJson['release-date'],
+      shortInfo = moviesJson['short-info'],
+      plot = moviesJson['plot'],
+      posterPath = moviesJson['poster_path'],
+      backgroundColor = moviesJson['backgroundColor'],
+      backdropImage = moviesJson['backdrop_image'],
+    );
   }
 }
